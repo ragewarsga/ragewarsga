f7a5519615c023b893a3df788aea8d1853908869
Jack <29169102+Jack5079@users.noreply.github.com>
2020-08-27 20:40:56 -0400

    add golang

---
+++ open/open.go
@@ -0,0 +1,27 @@
+package main
+
+import "os/exec"
+import "runtime"
+
+
+// open opens the specified URL in the default browser of the user.
+func open(url string) error {
+    var cmd string
+    var args []string
+
+    switch runtime.GOOS {
+    case "windows":
+        cmd = "cmd"
+        args = []string{"/c", "start"}
+    case "darwin":
+        cmd = "open"
+    default: // "linux", "freebsd", "openbsd", "netbsd"
+        cmd = "xdg-open"
+    }
+    args = append(args, url)
+    return exec.Command(cmd, args...).Start()
+}
+
+func main ()  {
+	open("https://ragewars.ga")
+}
