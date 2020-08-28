6cf17e91a6e3d941c8c27f503855c2e8173aad99
Jack <29169102+Jack5079@users.noreply.github.com>
2020-08-27 20:43:15 -0400

    add throw

--- open/open.cs
+++ open/open.cs
@@ -1,26 +1,27 @@
 using System.Threading;
+using static System.Console;
 
 public class Program
 {
   public static void Main(string[] args) => OpenBrowser("https://ragewars.ga/");
 
   public static void OpenBrowser(string url)
+  {
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
     {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-        Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
-        }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        {
-            Process.Start("xdg-open", url);
-        }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-        {
-            Process.Start("open", url);
-        }
-        else
-        {
-            // throw 
-        }
+      Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
     }
+    else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    {
+      Process.Start("xdg-open", url);
+    }
+    else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+    {
+      Process.Start("open", url);
+    }
+    else
+    {
+      WriteLine("tf... what os are you running");
+    }
+  }
 }
